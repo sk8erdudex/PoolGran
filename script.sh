a704dc0d07636d42af16eafef72acaa35057feb4
sk8erdudex <41309385+sk8erdudex@users.noreply.github.com>
2025-07-26 16:29:08 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# This whole script is running as root, but the SSH command below is executed as 'pool'
-sudo -u pool ssh -p 1995 steamuser@jariel.ddns.net 'date > /tmp/connected_from_nat_$(date +%s)'
+echo "$(date +'%F_%T')" > /tmp/last_script.log
