955e4499dc8b1cb98526540926225ea97b6a50bc
sk8erdudex <41309385+sk8erdudex@users.noreply.github.com>
2025-07-26 13:53:48 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,4 @@
-ssh -p 1995 steamuser@jariel.ddns.net "date > /tmp/connected_from_nat"
+#!/bin/bash
+
+# This whole script is running as root, but the SSH command below is executed as 'pool'
+sudo -u pool ssh -p 1995 steamuser@jariel.ddns.net 'date > /tmp/connected_from_nat_$(date +%s)'
